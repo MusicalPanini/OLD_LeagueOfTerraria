d938f1f1d29a3f38a09c1fb01dbf4890d307c392
Aidan Thorne <thaaidstar@gmail.com>
2019-11-06 19:30:48 +1100

    Added Necromancer and Ghoul npcs

---
+++ TerraLeague/NPCs/Ghoul.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using TerraLeague.Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TerraLeague.NPCs
+{
+    public class Ghoul : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ghoul");
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.BloodZombie];
+        }
+        public override void SetDefaults()
+        {
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 3;
+            npc.damage = 12;
+            npc.defense = 9;
+            npc.lifeMax = 55;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath2;
+            aiType = NPCID.BloodZombie;
+            animationType = NPCID.BloodZombie;
+            npc.scale = 1f;
+            base.SetDefaults();
+        }
+
+        public override bool PreAI()
+        {
+            Lighting.AddLight(npc.Center, new Color(5, 245, 150).ToVector3());
+
+            return base.PreAI();
+        }
+
+        public override void AI()
+        {
+            base.AI();
+        }
+
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+            base.OnHitPlayer(target, damage, crit);
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            base.HitEffect(hitDirection, damage);
+        }
+
+        public override void NPCLoot()
+        {
+            base.NPCLoot();
+        }
+    }
+}
