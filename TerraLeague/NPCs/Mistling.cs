9510d18f55dcc1eb715a25a9479caf2bb0f7afae
Aidan Thorne <thaaidstar@gmail.com>
2019-11-13 17:09:03 +1100

    Revert "Added gel drop"
    This reverts commit cb3b0a9aab0f92ae662006deba4c8bffb1f3c310.

--- TerraLeague/NPCs/Mistling.cs
+++ TerraLeague/NPCs/Mistling.cs
@@ -16,17 +16,16 @@ namespace TerraLeague.NPCs
         }
         public override void SetDefaults()
         {
-            npc.width = 24;
-            npc.height = 24;
-            npc.aiStyle = 2;
+            npc.width = 20;
+            npc.height = 20;
+            npc.aiStyle = 56;
             npc.damage = 35;
             npc.defense = 10;
-            npc.lifeMax = 300;
+            npc.lifeMax = 50;
             npc.noTileCollide = true;
             npc.noGravity = true;
             npc.HitSound = SoundID.NPCHit54;
             npc.DeathSound = SoundID.NPCHit52;
-            npc.aiStyle = NPCID.DemonEye;
             animationType = NPCID.DungeonSpirit;
             npc.value = 100;
             npc.knockBackResist = 0.2f;
@@ -41,23 +40,11 @@ namespace TerraLeague.NPCs
         {
             Lighting.AddLight(npc.Center, new Color(5, 245, 150).ToVector3());
 
-            for (int i = 0; i < 3; i++)
-            {
-                Dust dust = Dust.NewDustDirect(npc.position, npc.width, npc.height, 16, 0f, 0f, 100, new Color(67, 248, 175), Main.rand.Next(1, 3));
-                dust.alpha = 200;
-                dust.noGravity = true;
-                dust.velocity *= 0.1f;
-                dust.velocity += npc.velocity * 0.1f;
-                dust.position.X -= npc.velocity.X / 3f * (float)i;
-                dust.position.Y -= npc.velocity.Y / 3f * (float)i;
-            }
-
             return base.PreAI();
         }
 
         public override void AI()
         {
-            npc.rotation = npc.velocity.ToRotation() - MathHelper.PiOver2;
             base.AI();
         }
 
@@ -68,37 +55,11 @@ namespace TerraLeague.NPCs
 
         public override void HitEffect(int hitDirection, double damage)
         {
-            if (npc.life > 0)
-            {
-                int count = 0;
-                while ((double)count < damage / (double)npc.lifeMax * 50.0)
-                {
-                    int num618 = Dust.NewDust(npc.position, npc.width, npc.height, 16, 0f, 0f, 50, new Color(5, 245, 150), 1.5f);
-                    Dust dust = Main.dust[num618];
-                    dust.velocity *= 2f;
-                    Main.dust[num618].noGravity = true;
-                    count++;
-                }
-            }
-            else
-            {
-                for (int i = 0; i < 20; i++)
-                {
-                    int num620 = Dust.NewDust(npc.position, npc.width, npc.height, 16, 0f, 0f, 50, new Color(5, 245, 150), 1.5f);
-                    Dust dust = Main.dust[num620];
-                    dust.velocity *= 2f;
-                    Main.dust[num620].noGravity = true;
-                }
-                int num621 = Gore.NewGore(new Vector2(npc.position.X, npc.position.Y), new Vector2((float)hitDirection, 0f), mod.GetGoreSlot("Gores/MistPuff_1"), npc.scale);
-                Gore gore = Main.gore[num621];
-                gore.velocity *= 0.3f;
-            }
             base.HitEffect(hitDirection, damage);
         }
 
         public override void NPCLoot()
         {
-            Item.NewItem(npc.position, npc.width, npc.height, ItemType<DamnedSoul>(), 1);
             base.NPCLoot();
         }
     }
