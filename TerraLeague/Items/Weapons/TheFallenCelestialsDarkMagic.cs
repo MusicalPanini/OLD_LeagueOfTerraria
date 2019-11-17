8ad7759067e0ff22975287d95bac115a2347f884
Aidan Thorne <thaaidstar@gmail.com>
2019-11-17 19:08:59 +1100

    Updated The Fallen Celestials Dark Magic

--- TerraLeague/Items/Weapons/TheFallenCelestialsDarkMagic.cs
+++ TerraLeague/Items/Weapons/TheFallenCelestialsDarkMagic.cs
@@ -62,7 +62,7 @@ namespace TerraLeague.Items.Weapons
             else if (type == AbilityType.R)
             {
                 return "Unleash celestial chains upon near by enemies." +
-                    "\nAfter 3 seconds, if the chain is not broken, stun them for 4 seconds dealing damage again." +
+                    "\nAfter 3 seconds, if the chain is not broken, the enemy will be restuck and stunned for 4 seconds." +
                     "\nThe chain will break if the target moves too far away";
             }
             else
@@ -76,7 +76,7 @@ namespace TerraLeague.Items.Weapons
             if (type == AbilityType.E)
                 return (int)(1.5 * item.damage);
             else if (type == AbilityType.R)
-                return (int)(2 * item.damage);
+                return (int)(4 * item.damage);
             else
                 return base.GetAbilityBaseDamage(player, type);
         }
@@ -121,7 +121,7 @@ namespace TerraLeague.Items.Weapons
             if (type == AbilityType.E)
                 return 22;
             else if (type == AbilityType.R)
-                return 120;
+                return 70;
             else
                 return base.GetRawCooldown(type);
         }
@@ -205,33 +205,26 @@ namespace TerraLeague.Items.Weapons
 
         public override void SetDefaults()
         {
-            item.damage = 50;
+            item.damage = 26;
             item.noMelee = true;
             item.magic = true;
-            item.useTime = 4;
-            item.useAnimation = 12;
-            item.shootSpeed = 14;
-            item.mana = 8;
+            item.useTime = 80;
+            item.useAnimation = 80;
+            item.mana = 40;
             item.rare = 5;
             item.value = 300000;
             item.width = 28;
             item.height = 32;
-            item.knockBack = 1;
-            item.UseSound = new Terraria.Audio.LegacySoundStyle(2, 34);
+            item.knockBack = 0;
+            item.UseSound = SoundID.Item20;
             item.useStyle = 5;
-            item.shoot = ProjectileID.AmethystBolt;
-            item.autoReuse = true;
+            item.shoot = ProjectileType<TormentedShadow>();
         }
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            item.useTime = 5;
-            item.useAnimation = 10;
-            item.shootSpeed = 14;
-            item.mana = 6;
-            item.UseSound = new Terraria.Audio.LegacySoundStyle(2, 34);
-
-            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
+            Projectile.NewProjectileDirect(Main.MouseWorld, Vector2.Zero, type, damage, knockBack, player.whoAmI);
+            return false;
         }
 
         public override void AddRecipes()
