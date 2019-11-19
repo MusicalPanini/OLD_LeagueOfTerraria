1c11e14617ffb67260dedd1aa89252441bf8f731
Aidan Thorne <thaaidstar@gmail.com>
2019-11-19 15:07:10 +1100

    Added Bone Skewer

---
+++ TerraLeague/Buffs/DeathFromBelowRefresh.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using TerraLeague.NPCs;
+
+namespace TerraLeague.Buffs
+{
+    public class DeathFromBelowRefresh : ModBuff
+    {
+        public bool initial = true;
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Death From Below");
+            Description.SetDefault("Can cast Death From Below again for free");
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<PLAYERGLOBAL>().deathFromBelowRefresh = true;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+        }
+    }
+}
