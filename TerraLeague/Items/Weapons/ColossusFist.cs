f047f39baf8ac25818d8158943289d92abfbff95
Aidan Thorne <thaaidstar@gmail.com>
2019-11-20 17:57:55 +1100

    Added Winds of War to Colossus Fist

--- TerraLeague/Items/Weapons/ColossusFist.cs
+++ TerraLeague/Items/Weapons/ColossusFist.cs
@@ -1,17 +1,123 @@
 ﻿using Microsoft.Xna.Framework;
 using TerraLeague.Projectiles;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
 namespace TerraLeague.Items.Weapons
 {
-    public class ColossusFist : ModItem
+    public class ColossusFist : AbilityItem
     {
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Colossus Fist");
+            Tooltip.SetDefault("");
+        }
+
+        public override string GetWeaponTooltip()
+        {
+            return "";
+        }
+
+        public override string GetQuote()
+        {
+            return "Time to make an impact!";
+        }
+
+        public override string GetAbilityName(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return "Winds of War";
+            else
+                return base.GetAbilityName(type);
+        }
+
+        public override string GetIconTexturePath(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return "AbilityImages/WindsofWar";
+            else
+                return base.GetIconTexturePath(type);
+        }
+
+        public override string GetAbilityTooltip(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+            {
+                return "Create a massive tornado at the target location.";
+            }
+            else
+            {
+                return base.GetAbilityTooltip(type);
+            }
+        }
+
+        public override int GetAbilityBaseDamage(Player player, AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return (int)(item.damage * 0.5);
+            else
+                return base.GetAbilityBaseDamage(player, type);
+        }
+
+        public override int GetAbilityScalingAmount(Player player, AbilityType type, DamageType dam)
+        {
+            if (type == AbilityType.Q)
+            {
+                if (dam == DamageType.MAG)
+                    return 20;
+            }
+            return base.GetAbilityScalingAmount(player, type, dam);
+        }
+
+        public override int GetBaseManaCost(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return 30;
+            else
+                return base.GetBaseManaCost(type);
+        }
+
+        public override string GetDamageTooltip(Player player, AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return GetAbilityBaseDamage(player, type) + " + " + GetScalingTooltip(player, type, DamageType.MAG) + " magic damage";
+            else
+                return base.GetDamageTooltip(player, type);
+        }
+
+        public override int GetRawCooldown(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return 10;
+            else
+                return base.GetRawCooldown(type);
+        }
+
+        public override void DoEffect(Player player, AbilityType type)
+        {
+            if (type == AbilityType.Q)
+            {
+                if (CheckIfNotOnCooldown(player, type) && player.CheckMana(GetScaledManaCost(type), true))
+                {
+                    Vector2 position = Main.MouseWorld;
+                    
+                    int projType = ProjectileType<WindsofWar>();
+                    int damage = GetAbilityBaseDamage(player, type) + GetAbilityScalingDamage(player, type, DamageType.MEL);
+                    int knockback = 0;
+
+                    SetAnimation(player, 10, 10, position);
+                    DoEfx(player, type);
+                    Projectile.NewProjectile(position, new Vector2(0, 1000), projType, damage, knockback, player.whoAmI, 6);
+                    SetCooldowns(player, type);
+                }
+            }
+            else
+            {
+                base.DoEffect(player, type);
+            }
         }
 
         public override void SetDefaults()
@@ -55,6 +161,24 @@ namespace TerraLeague.Items.Weapons
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
+        }
+
+        public override bool GetIfAbilityExists(AbilityType type)
+        {
+            if (type == AbilityType.Q)
+                return true;
+            return base.GetIfAbilityExists(type);
+        }
+
+        public override void Efx(Player player, AbilityType type)
+        {
+            if (type == AbilityType.Q)
+            {
+                Microsoft.Xna.Framework.Audio.SoundEffectInstance sound = Main.PlaySound(new LegacySoundStyle(2, 117).WithPitchVariance(0.8f), player.Center);
+                if (sound != null)
+                    sound.Pitch = 0f;
+            }
+
         }
     }
 }
---
+++ TerraLeague/Projectiles/WindsofWar.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerraLeague.Projectiles
+{
+    public class WindsofWar : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Winds of War");
+            Main.projFrames[projectile.type] = 6;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 162;
+            projectile.height = 42;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.alpha = 255;
+            projectile.scale = 1f;
+            projectile.timeLeft = 180;
+            projectile.magic = true;
+            projectile.tileCollide = true;
+        }
+
+        public override void AI()
+        {
+            if (projectile.timeLeft > 100)
+            {
+                if (projectile.alpha > 0)
+                {
+                    projectile.alpha -= 10;
+                }
+                if (projectile.alpha < 100)
+                {
+                    projectile.alpha = 100;
+                }
+            }
+            else if (projectile.timeLeft < 155/10f)
+            {
+                projectile.alpha += 10;
+
+                if (projectile.alpha > 255)
+                {
+                    projectile.alpha = 255;
+                }
+            }
+
+                Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 31, 0, -3, projectile.alpha/2, default(Color), 0.5f);
+
+            if ((int)projectile.ai[0] > 0 && projectile.timeLeft == 175)
+            {
+                Projectile.NewProjectileDirect(projectile.Center + new Vector2(0, -42), Vector2.Zero, projectile.type, projectile.damage, projectile.knockBack, projectile.owner, (int)projectile.ai[0] - 1);
+            }
+
+            AnimateProjectile();
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            base.Kill(timeLeft);
+        }
+
+        public void AnimateProjectile()
+        {
+            projectile.frameCounter++;
+            if (projectile.frameCounter >= 4)
+            {
+                projectile.frame++;
+                projectile.frame %= 6;
+                projectile.frameCounter = 0;
+            }
+        }
+    }
+}
