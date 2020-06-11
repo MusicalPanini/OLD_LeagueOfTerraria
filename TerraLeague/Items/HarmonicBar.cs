477c20d83cba31ad288162b2795c0fbaba9bc2da
Aidan Thorne <thaaidstar@gmail.com>
2020-06-11 12:55:20 +1000

    Added Harmonic Bar

---
+++ TerraLeague/Items/HarmonicBar.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TerraLeague.Items
+{
+    public class HarmonicBar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Harmonic Bar");
+            Tooltip.SetDefault("'A balance of peace and power'");
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            item.maxStack = 99;
+            item.width = 30;
+            item.height = 24;
+            item.uniqueStack = false;
+            item.rare = ItemRarityID.Orange;
+            item.value = 22000;
+            item.createTile = TileType<Tiles.HarmonicBarTile>();
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<ManaBar>(), 4);
+            recipe.AddIngredient(ItemID.SoulofLight, 2);
+            recipe.AddIngredient(ItemID.SoulofNight, 2);
+            recipe.AddTile(TileID.AdamantiteForge);
+            recipe.SetResult(this, 4);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ TerraLeague/Tiles/HarmonicBarTile.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.ObjectData;
+using TerraLeague.Items;
+using static Terraria.ModLoader.ModContent;
+
+namespace TerraLeague.Tiles
+{
+    public class HarmonicBarTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileShine[Type] = 1100;
+            Main.tileSolid[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Harmonic Bar");
+            AddMapEntry(new Color(179, 0, 237), name);
+
+            dustType = 27;
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            Tile t = Main.tile[i, j];
+            int style = t.frameX / 18;
+            if (style == 0) 
+            {
+                Item.NewItem(i * 16, j * 16, 16, 16, ItemType<HarmonicBar>());
+            }
+            return base.Drop(i, j);
+        }
+    }
+}
