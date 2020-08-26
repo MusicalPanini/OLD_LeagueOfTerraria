46a94c38ed477c73dab2f10e02496eb5bb72bffd
Aidan Thorne <thaaidstar@gmail.com>
2020-08-26 15:39:27 +1000

    Added Zz'Rot Portal

---
+++ TerraLeague/Items/CompleteItems/Zzrot.cs
@@ -0,0 +1,98 @@
+﻿using TerraLeague.Items.AdvItems;
+using TerraLeague.Items.BasicItems;
+using TerraLeague.Items.CustomItems;
+using TerraLeague.Items.CustomItems.Actives;
+using TerraLeague.Items.CustomItems.Passives;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace TerraLeague.Items.CompleteItems
+{
+    public class Zzrot : LeagueItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Zz'Rot Portal");
+            Tooltip.SetDefault("Increases armor by 3" +
+                "\nIncreases resist by 4" +
+                "\nIncreases your max number of sentries" +
+                "\nIncreases life regeneration by 2");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.value = 180000;
+            item.rare = ItemRarityID.Pink;
+            item.accessory = true;
+        }
+
+        public override bool CanEquipAccessory(Player player, int slot)
+        {
+            if (slot >= 3 && slot <= 8)
+                player.GetModPlayer<PLAYERGLOBAL>().accessoryStat[slot - 3] = (int)(60 * player.GetModPlayer<PLAYERGLOBAL>().Cdr * 60);
+            return true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.meleeSpeed += 0.15f;
+            player.maxMinions += 1;
+            player.GetModPlayer<PLAYERGLOBAL>().resist += 6;
+            player.GetModPlayer<PLAYERGLOBAL>().armor += 4;
+            player.lifeRegen += 2;
+
+            base.UpdateAccessory(player, hideVisual);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<RaptorCloak>(), 1);
+            recipe.AddIngredient(ItemType<NegatronCloak>(), 1);
+            recipe.AddIngredient(ItemType<VoidFragment>(), 100);
+            recipe.AddIngredient(ItemID.SoulofSight, 10);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override Active GetActive()
+        {
+            return new VoidCaller(20, 3, 15, 60);
+        }
+
+        public override string GetStatText()
+        {
+            int slot = TerraLeague.FindAccessorySlotOnPlayer(Main.LocalPlayer, this);
+
+            if (slot != -1)
+            {
+                if ((int)GetStatOnPlayer(Main.LocalPlayer) > 0 && Main.LocalPlayer.GetModPlayer<PLAYERGLOBAL>().ActivesAreActive[slot])
+                    return ((int)GetStatOnPlayer(Main.LocalPlayer) / 60).ToString();
+                else
+                    return "";
+            }
+            else
+                return "";
+        }
+
+        public override bool OnCooldown(Player player)
+        {
+            int slot = TerraLeague.FindAccessorySlotOnPlayer(Main.LocalPlayer, this);
+
+            if (slot != -1)
+            {
+                if ((int)GetStatOnPlayer(Main.LocalPlayer) > 0 || !Main.LocalPlayer.GetModPlayer<PLAYERGLOBAL>().ActivesAreActive[slot])
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ TerraLeague/Items/CustomItems/Actives/VoidCaller.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using TerraLeague.Projectiles;
+using Terraria;
+using static Terraria.ModLoader.ModContent;
+
+namespace TerraLeague.Items.CustomItems.Actives
+{
+    public class VoidCaller : Active
+    {
+        int baseDamage;
+        int baseMinions;
+        int cooldown;
+        int sumScaling;
+
+        public VoidCaller(int BaseDamage, int BaseMinions, int SumScaling, int Cooldown)
+        {
+            baseDamage = BaseDamage;
+            baseMinions = BaseMinions;
+            sumScaling = SumScaling;
+            cooldown = Cooldown;
+        }
+
+        public override string Tooltip(Player player, LeagueItem modItem)
+        {
+            PLAYERGLOBAL modPlayer = player.GetModPlayer<PLAYERGLOBAL>();
+
+            return "[c/ff4d4d:Active: VOID CALLER -] [c/ff8080:Summon a Zz'Rot portal at your cursor.]" +
+                "\n[c/ff8080:It ejects 3 + Max Minions (" + player.maxMinions + ") Zz'Rots every second for 5 seconds. The Zz'rots deal] " + baseDamage + " + [c/" + TerraLeague.SUMColor + ":" + (int)(modPlayer.SUM * sumScaling / 100d) + "] [c/ff8080:damage]" +
+                "\n[c/cc0000:" + (int)(cooldown * modPlayer.cdrLastStep) + " second cooldown]";
+        }
+
+        public override void DoActive(Player player, LeagueItem modItem)
+        {
+            PLAYERGLOBAL modPlayer = player.GetModPlayer<PLAYERGLOBAL>();
+
+            if (modItem.GetStatOnPlayer(player) <= 0)
+            {
+                player.FindSentryRestingSpot(ProjectileType<Item_ZzrotPortal>(), out int xPos, out int yPos, out int yDis);
+                Projectile.NewProjectile((float)xPos, (float)(yPos - yDis), 0f, 0f, ProjectileType<Item_ZzrotPortal>(), baseDamage + (int)(modPlayer.SUM * sumScaling * 0.01f), 2, player.whoAmI, baseMinions);
+                modPlayer.FindAndSetActiveStat(this, (int)(cooldown * modPlayer.Cdr * 60));
+
+                Efx(player);
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                    PacketHandler.SendActiveEfx(-1, player.whoAmI, player.whoAmI, modItem.item.type);
+            }
+        }
+
+        public override void Efx(Player user)
+        {
+            Main.PlaySound(new Terraria.Audio.LegacySoundStyle(2, 113), user.MountedCenter);
+            base.Efx(user);
+        }
+
+        public override void PostPlayerUpdate(Player player, LeagueItem modItem)
+        {
+            AddStat(player, modItem, cooldown * 60, -1, true);
+            base.PostPlayerUpdate(player, modItem);
+        }
+    }
+}
+
