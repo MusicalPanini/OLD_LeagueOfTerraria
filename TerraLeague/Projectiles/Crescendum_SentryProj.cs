b6934efe88eede8e85ff3b8c1bac7e86a29b2f86
Aidan Thorne <thaaidstar@gmail.com>
2020-08-31 16:03:27 +1000

    Fixed bug when sentry would kill while projectile was out

--- TerraLeague/Projectiles/Crescendum_SentryProj.cs
+++ TerraLeague/Projectiles/Crescendum_SentryProj.cs
@@ -43,6 +43,11 @@ namespace TerraLeague.Projectiles
                 }
             }
 
+            if (sentry == null)
+            {
+                projectile.Kill();
+            }
+
             if (projectile.soundDelay == 0)
             {
                 projectile.soundDelay = 8;
