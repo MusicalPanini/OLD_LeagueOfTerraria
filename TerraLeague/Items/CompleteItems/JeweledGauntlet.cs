74dafec3bb8095c48c9d1932cdd974851b065726
Aidan Thorne <thaaidstar@gmail.com>
2020-09-02 13:21:40 +1000

    Buffed Jeweled Gauntlet

--- TerraLeague/Items/CompleteItems/JeweledGauntlet.cs
+++ TerraLeague/Items/CompleteItems/JeweledGauntlet.cs
@@ -14,8 +14,10 @@ namespace TerraLeague.Items.CompleteItems
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Jeweled Gauntlet");
-            Tooltip.SetDefault("5% increased damage" +
-                "\n8% increased crit chance");
+            Tooltip.SetDefault("8% increased damage" +
+                "\n8% increased crit chance" +
+                "\nIncreases maximum mana by 40" +
+                "\nAbility cooldown reduced by 10%");
         }
 
         public override void SetDefaults()
@@ -28,10 +30,12 @@ namespace TerraLeague.Items.CompleteItems
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.meleeDamage += 0.05f;
-            player.rangedDamage += 0.05f;
-            player.magicDamage += 0.05f;
-            player.GetModPlayer<PLAYERGLOBAL>().TrueMinionDamage += 0.05;
+            player.meleeDamage += 0.08f;
+            player.rangedDamage += 0.08f;
+            player.magicDamage += 0.08f;
+            player.GetModPlayer<PLAYERGLOBAL>().TrueMinionDamage += 0.08;
+            player.GetModPlayer<PLAYERGLOBAL>().Cdr -= 0.1;
+            player.statManaMax2 += 40;
 
             base.UpdateAccessory(player, hideVisual);
         }
@@ -40,12 +44,11 @@ namespace TerraLeague.Items.CompleteItems
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemType<BFSword>(), 1);
-            recipe.AddIngredient(ItemType<LargeRod>(), 1);
             recipe.AddIngredient(ItemType<BrawlersGlove>(), 1);
+            recipe.AddIngredient(ItemType<LostChapter>(), 1);
             recipe.AddIngredient(ItemID.DestroyerEmblem, 1);
             recipe.AddIngredient(ItemID.SoulofMight, 6);
             recipe.AddIngredient(ItemID.HallowedBar, 12);
-            recipe.AddIngredient(ItemID.Deathweed, 2);
             recipe.AddTile(TileID.MythrilAnvil);
             recipe.SetResult(this);
             recipe.AddRecipe();
@@ -58,7 +61,7 @@ namespace TerraLeague.Items.CompleteItems
 
         public override Passive GetSecondaryPassive()
         {
-            return null;
+            return new Haste();
         }
 
         public override string GetStatText()
