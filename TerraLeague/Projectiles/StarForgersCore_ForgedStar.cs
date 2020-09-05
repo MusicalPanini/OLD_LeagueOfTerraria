07d7862f13426eb95f551900ebb3518e767fd045
Aidan Thorne <thaaidstar@gmail.com>
2020-09-05 13:15:51 +1000

    Buffed Forged Star damage for each start the player controls

--- TerraLeague/Projectiles/StarForgersCore_ForgedStar.cs
+++ TerraLeague/Projectiles/StarForgersCore_ForgedStar.cs
@@ -96,8 +96,8 @@ namespace TerraLeague.Projectiles
 
         public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
         {
-            //int count = Main.player[projectile.owner].ownedProjectileCounts[projectile.type];
-            //damage = (int)(damage * (1 + ((count - 1) * 0.05)));
+            int count = Main.player[projectile.owner].ownedProjectileCounts[projectile.type];
+            damage = (int)(damage * (1 + ((count - 1) * 0.1)));
 
             base.ModifyHitNPC(target, ref damage, ref knockback, ref crit, ref hitDirection);
         }
