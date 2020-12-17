ae62e3ea6dc2c9d926faa876536a87358cc0d36f
Aidan Thorne <thaaidstar@gmail.com>
2020-12-17 13:51:53 +1100

    Fixed Crescendum having right click functionality

--- TerraLeague/Items/Weapons/Crescendum.cs
+++ TerraLeague/Items/Weapons/Crescendum.cs
@@ -72,7 +72,7 @@ namespace TerraLeague.Items.Weapons
 
         public override bool AltFunctionUse(Player player)
         {
-            return true;
+            return false;
         }
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
